ec436d81aa5d971d1acead9200f0f2081e7c3fc7
sanpedrofabianezequiel <sanpedrofabianezequiel@gmail.com>
2021-10-13 00:35:40 -0300

    Update Calendar UI

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { AppRouter } from './router/AppRouter'
+import './styles.css';
 
 export const CalendarApp = () => {
     return (
--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Navbar } from '../ui/Navbar'
+import {Calendar,momentLocalizer} from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { messages } from '../../helpers/calendar-message-es';
 
+
+import 'moment/locale/es';
+import { CalendarEvent } from './CalendarEvent';
+moment.locale('es');
+const localizer = momentLocalizer(moment) // or globalizeLocalizer
+
+
+const myEventsList = [{
+    title:'Cumpleaños del jefe',
+    start: moment().toDate(),
+    end:moment().add(2,'hours').toDate(),
+    bgcolor:'#fafafa',
+    notes:'Comprar el pastel',
+    user:{
+        _id:'123',
+        name:'Ezequiel'
+    }
+}]
 export const CalendarScreen = () => {
+
+    const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month');
+
+    const onDoubleClick = (e)=>{
+        console.log(e);
+    }
+    const onSelectEvent = (e)=>{
+        console.log(e);
+    }
+    const onViewChange = (e)=>{
+        //Cuando cambia la vista seteo en local storage
+        setLastView(e);
+        localStorage.setItem('lastView',e);
+    }
+
+    const eventStyleGetter=(event, start,end, isSelected)=>{
+        const styles = {
+            backgroundColor:'#367CF7',
+            borderRadius:'0px',
+            opacity:0.8,
+            display:'block',
+            color:'white'
+        }
+        return {
+            styles
+        }
+    };
     return (
-        <div>
+        <div className="calendar-screen">
            <Navbar/>
+           <Calendar
+            localizer={localizer}
+            events={myEventsList}
+            startAccessor="start"
+            endAccessor="end"
+            messages={messages}
+            eventPropGetter={eventStyleGetter}
+            components={{
+                event:CalendarEvent
+            }}
+            onDoubleClickEvent={onDoubleClick}
+            onSelectEvent={onSelectEvent}
+            onView={onViewChange}
+            view={lastView}
+            />
         </div>
     )
 }
